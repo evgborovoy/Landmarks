2e7851dd276ab8ccb4f5ce92827fb330780b2cf1
Evgeniy <borovoyevgen@gmail.com>
2024-11-18 03:06:21 +0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Evgeniy Borovoy on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
