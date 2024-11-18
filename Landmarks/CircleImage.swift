b056f1a2eee8af7b8b1cc6602192df9ea0588dbe
Evgeniy <borovoyevgen@gmail.com>
2024-11-18 15:35:42 +0400

    Create simple representation about place

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,22 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Evgeniy Borovoy on 11/18/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }.shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,15 +10,30 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            MapView().frame(height: 300)
+            CircleImage().offset(y: -140).padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock").font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California, USA")
+                }.font(.subheadline).foregroundStyle(.secondary)
+                Divider()
+                Text("About Turtle Rock").font(.title2)
+                Text("Description text about place")
+                
+            }.padding()
+            Spacer()
+            
+            
         }
-        .padding()
     }
 }
+    
+    #Preview {
+        ContentView()
+    }
 
-#Preview {
-    ContentView()
-}
